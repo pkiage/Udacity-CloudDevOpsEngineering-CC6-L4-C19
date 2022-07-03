f0caa8374373f3e3cf8d517ef0a036ce51910344
pkiage <paul.r.kiage@gmail.com>
2022-07-03 11:38:35 +0300

    chore: update stack create

--- shell-scripts/stack-create.sh
+++ shell-scripts/stack-create.sh
@@ -1,2 +1,1 @@
-# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+aws cloudformation deploy --template-file $1 --stack-name demoStack --region us-west-2
