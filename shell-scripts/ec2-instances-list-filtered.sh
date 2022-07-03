7012b254a120220e7f5f038be68322110a753a7e
pkiage <paul.r.kiage@gmail.com>
2022-07-03 12:04:25 +0300

    chore: add query parameter to describe-instances

--- shell-scripts/ec2-instances-list-filtered.sh
+++ shell-scripts/ec2-instances-list-filtered.sh
@@ -1,2 +1,4 @@
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
-aws ec2 describe-instances --filters file://$1
+aws ec2 describe-instances --filters file://$1 \
+    --query 'Reservations[*].Instances[*].{PublicDnsName:PublicDnsName,InstanceId:InstanceId,SubnetId:SubnetId,SecurityGroups:SecurityGroups}' \
+    --output json
